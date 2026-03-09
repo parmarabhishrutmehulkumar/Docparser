a76dc7e473e7c0e3ebeec4ee89f7c617b5071db4
Abhi <parmarabhishrut@gmail.com>
2026-03-09 10:50:06 +0530

    New rules applied!

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ def run_fuzzy_model(cgpa: float, test_score: float, admission_strictness: float)
     for r in [r2,r3,r4,r5,r6,r7,r8,r9,
           r10,r11,r12,r13,r14,r15,r16,r17,r18,
           r19,r20,r21,r22,r23,r24,r25,r26,r27]:
-          aggregated = np.fmax(aggregated, r)
+        aggregated = np.fmax(aggregated, r)
    
     try:
         final_score = fuzz.defuzz(x_suitability, aggregated, "centroid")
